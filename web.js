0af7d1ff2c9d0079f65ed69234204a97bba8f4b0
jdhnet <jdhnet@outlook.com>
2013-08-02 18:12:37 +0000

    modified web.js for displaying images

--- web.js
+++ web.js
@@ -9,6 +9,8 @@ app.get('/', function(request, response) {
     response.send(content);
 });
 
+app.use("/img", express.static(__dirname + "/img"));
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
